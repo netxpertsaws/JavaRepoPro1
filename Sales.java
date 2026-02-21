656796119aef44e5680756b695f948e625bd29cf
ravi <ravi@yahoo.com>
2026-02-21 13:45:09 +0000

    11th line Modified Sales 11th Line Added

--- Sales.java
+++ Sales.java
@@ -9,4 +9,7 @@ System.out.println("It is awesome!");
 System.out.println("This sentence will work!");
 // This is a comment
 System.out.println("Hello World");
-System.out.println("Hello World"); // This is a comment
+String name = "John";
+System.out.println(name);
+int myNum = 15;
+System.out.println(myNum);
